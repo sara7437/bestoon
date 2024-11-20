52faecda417a9fa3bddfec2f09ddf40f7de564da
sara7437 <imaneh_7437@yahoo.com>
2024-11-20 22:16:08 +0330

    ignore create

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense,Income
 
 # ثبت مدل Expense در پنل مدیریت
 admin.site.register(Expense)
+admin.site.register(Income)
